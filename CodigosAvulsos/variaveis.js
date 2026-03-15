6acb67375fc32ba88424f858ee4be8be71c1bb3a
lividpower <matheusmontenegro100@gmail.com>
2026-03-15 10:42:44 -0300

    docs(escopo): adiciona exemplo de acesso a variável global

--- CodigosAvulsos/variaveis.js
+++ CodigosAvulsos/variaveis.js
@@ -2,7 +2,8 @@ var teste = "variável_1";
 
 (function escopoVariavel() {
     var teste = "variável_2";
-    console.log(teste);
+    console.log("acessando a variável local:", teste); //aqui utiliza-se da variável "teste" declarada dentro do escopo da função
+    console.log("acessando a variável global:", window.teste); //neste caso, estamos forçando a utilização da variável "teste" declarada dentro do escopo global
 })();
 
 var teste2 = "x";
