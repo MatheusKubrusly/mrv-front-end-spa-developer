cf3edf2e2d005c06929737b802095ad228675bea
lividpower <matheusmontenegro100@gmail.com>
2026-03-26 00:59:23 -0300

    Como referenciar atributos de objetos da forma correta
    Neste commit, demonstrei como posso referenciar os atributos de um objeto por meio de seus métodos tanto na linguagem Java quanto em JavaScript

--- CodigosAvulsos/Teste.java
+++ CodigosAvulsos/Teste.java
@@ -1,3 +1,8 @@
 public class Teste {
-    String name = "nome presente no objeto pai";
+    private String name = "nome presente no objeto pai";
+
+    public String getName() {
+        return name; //perceba que eu não preciso utilizar de "this", mas no JavaScript parece ser necessário!
+        //apenas precisaríamos utilizar do "this" caso houvesse uma variável local com o mesmo nome da variável de instância, ou seja, a variável "name" local ao método "getName()".
+    }
 }
