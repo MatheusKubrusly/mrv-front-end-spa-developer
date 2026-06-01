d94bb35a4bcd7b9971354474cfa94fd7f67ae36f
MatheusKubrusly <matheusmontenegro100@gmail.com>
2026-05-31 22:27:53 -0300

    Reformulando a estrutura deste meu repositório
    A ideia por trás desta reformulação é centralizar todos os meus projetos e cursos desenvolvidos durante as minhas formações dentro da plataforma de cursos DIO

---
+++ desenvolvimento-web/CodigosAvulsos/teste.js
@@ -0,0 +1,13 @@
+idUser = document.getElementById("userId");
+console.log(idUser);
+console.log(idUser.value);
+
+//console.log(window.document);
+console.log(document);
+//estamos nos referindo a mesma variável...
+childrens = document.children;
+console.log(childrens);
+inputTag = document.getElementsByTagName("input"); // retorna uma HTMLCollection
+console.log(inputTag[0].value);
+
+
